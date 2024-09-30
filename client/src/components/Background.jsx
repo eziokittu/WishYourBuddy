25ae70b4fe9604040a3d66e636d674b4e3b64f76
Bodhisatta Bhattacharjee <bodhikittu@gmail.com>
2024-10-01 02:14:40 +0530

    1st

---
+++ client/src/components/Background.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+import particlesData from '../data/particlesData2.json';
+
+const Background = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo( () => (particlesData),[]);
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <div className="fixed w-full h-full top-0 z-0">
+	</div>;
+};
+
+export default Background;
